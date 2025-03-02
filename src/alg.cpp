b564fe4ec30a4cb243b40e0555b45eca4cf3e21b
vkupriyanova <vika2018arz@gmail.com>
2025-03-02 10:27:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,10 +18,10 @@ uint64_t nPrime(uint64_t n) {
   uint64_t number = 5;
   while (count < n) {
     if (checkPrime(number)) count++;
-    if (count == n) break;
+    if (count == n) return number;
     number += 2;
   }
-  return number-2;
+  return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
@@ -44,7 +44,7 @@ uint64_t sumPrime(uint64_t hbound) {
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t count = 0;
   for (uint64_t i = lbound; i < hbound-1; i++) {
-      if (checkPrime(i) && checkPrime(i+2)) count++;
+    if (checkPrime(i) && checkPrime(i+2)) count++;
   }
   return count;
 }
